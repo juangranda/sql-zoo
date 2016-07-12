1b1fe20226c5cb89350e8155361c3fffd7dab372
Juan Granda <juanfromecuador@gmail.com>
2016-07-12 18:40:37 -0400

    Create names.sql

---
+++ names.sql
@@ -0,0 +1,21 @@
+NAMES
+
+SELECT name FROM world
+  WHERE name LIKE 'Y%'
+  
+  SELECT name FROM world
+  WHERE name LIKE '%Y'
+  
+  SELECT name FROM world
+  WHERE name LIKE '%X%'
+  
+  SELECT name FROM world
+  WHERE name LIKE '%LAND'
+  
+  SELECT name FROM world
+  WHERE name LIKE 'C%ia'
+  
+  SELECT name FROM world
+  WHERE name LIKE '%oo%'
+  
+  
