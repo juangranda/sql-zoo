fe5ddd5598dbe9b0674553121d7539e4b783c16c
Juan Granda <juanfromecuador@gmail.com>
2016-07-13 11:09:25 -0400

    Update SelectFromNobel.sql

--- SelectFromNobel.sql
+++ SelectFromNobel.sql
@@ -47,3 +47,9 @@ WHERE winner LIKE 'Eugene O''neill'
 SELECT winner, yr, subject FROM nobel
 WHERE winner LIKE 'Sir%'
 
+--Show the 1984 winners and subject ordered by subject and winner name; but list Chemistry and Physics last.
+SELECT winner, subject
+  FROM nobel
+ WHERE yr=1984
+ ORDER BY subject IN ('Physics','Chemistry'), subject, winner
+
