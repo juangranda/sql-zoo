c3fd1eb05053e7318c7acacacd655930e8d79f94
Juan Granda <juanfromecuador@gmail.com>
2016-07-12 20:27:02 -0400

    Update names.sql

--- names.sql
+++ names.sql
@@ -18,4 +18,37 @@ SELECT name FROM world
   SELECT name FROM world
   WHERE name LIKE '%oo%'
   
+  SELECT name FROM world
+  WHERE name LIKE '%a%a%a%'
+  
+  SELECT name FROM world
+ WHERE name LIKE '_t%'
+ORDER BY name
+
+SELECT name FROM world
+ WHERE name LIKE '%o__o%'
+ 
+ SELECT name FROM world
+ WHERE name LIKE '____'
+ 
+ SELECT name
+  FROM world
+ WHERE name LIKE capital
+ 
+ SELECT name
+  FROM world
+ WHERE capital LIKE concat(name, '%City%')
+ 
+ SELECT capital, name
+FROM world
+WHERE capital LIKE concat(name, '%')
+
+SELECT name, capital
+FROM world
+WHERE capital LIKE concat(name, '_%')
+
+SELECT name, REPLACE(capital,name,'')
+FROM world
+WHERE capital LIKE concat(name, '_%')
+  
   
